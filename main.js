c622c53588c6efc1471a999e34d14c9e5683ef55
lordelogos <paulehiks@gmail.com>
2023-03-10 12:20:35 +0100

    feat: commented code logic, added assets to gitignore

--- main.js
+++ main.js
@@ -7,28 +7,35 @@ const context = canvas.getContext("2d");
 // application logic
 const frameCount = 64;
 const currentFrame = (index) => {
-  return `/assets/${index.toString().padStart(4, "0")}.png`;
+  // path to assets
+  // Demo can be found in assets folder
+  // return `/assets/${index.toString().padStart(4, "0")}.png`;
+  return `https://www.apple.com/105/media/us/airpods-pro/2022/d2deeb8e-83eb-48ea-9721-f567cf0fffa8/anim/hero/large/${index
+    .toString()
+    .padStart(4, "0")}.png`;
 };
 
 const preloadImages = () => {
+  // loop through all the frames and load the images
   for (let i = 1; i < frameCount + 1; i++) {
     const img = new Image();
     img.src = currentFrame(i);
   }
 };
 
+// change the image drawn on the canvas
 const updateImage = (index) => {
   img.src = currentFrame(index);
+  // clean up the prev image for transparent pngs
+  // also increase performance
+  context.clearRect(0, 0, canvas.width, canvas.height);
   context.drawImage(img, 0, 0, canvas.width, canvas.height);
 };
 
-// load images
-preloadImages();
-
+// draw the first image on page load
 const img = new Image();
 img.src = currentFrame(1);
 img.onload = function () {
-  context.clearRect(0, 0, canvas.width, canvas.height);
   context.drawImage(img, 0, 0, canvas.width, canvas.height);
 };
 
@@ -45,6 +52,9 @@ window.addEventListener("scroll", () => {
   );
   requestAnimationFrame(() => updateImage(frameIndex + 1));
 });
+
+// load images
+preloadImages();
 
 // logic ends
 document.body.appendChild(canvas);
